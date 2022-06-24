2e0aa4e1c78ded98ed3929adcddc6026f13d40f0
Slava Dobrov <dobrov.slava@yandex.ru>
2022-06-24 22:44:14 +0300

    Added class Palindrome. Program is working well.

--- src/main/java/Seminars/Second/Palindrome.java
+++ src/main/java/Seminars/Second/Palindrome.java
@@ -2,11 +2,17 @@
  * Определить является ли строка палиндромом
  */
 package Seminars.Second;
+import java.util.Objects;
 import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
-        System.out.println(userInsert());
-
+        String currentString = userInsert();
+        if(isPalindrome(currentString)) {
+            System.out.printf("String %s is Palindrome", currentString);
+        }
+        else {
+            System.out.printf("String %s is not Palindrome", currentString);
+        }
     }
 
     public static String userInsert() {
@@ -14,5 +20,14 @@ public class Palindrome {
         Scanner input = new Scanner(System.in);
         System.out.print("Input string: ");
         return input.nextLine();
+
+    }
+
+    public static boolean isPalindrome(String str) {
+        String[] array = str.split("");
+        for(int i = 0; i <= array.length/2; i++) {
+            if(!Objects.equals(array[i], array[array.length - 1 - i])) { return false; }
+        }
+        return true;
     }
 }
