75511711c33a94ddf280533e22a43db13a7d6baa
Slava Dobrov <dobrov.slava@yandex.ru>
2022-06-24 22:14:39 +0300

    Added method userInsert

--- src/main/java/Seminars/Second/Palindrome.java
+++ src/main/java/Seminars/Second/Palindrome.java
@@ -2,6 +2,17 @@
  * Определить является ли строка палиндромом
  */
 package Seminars.Second;
-
+import java.util.Scanner;
 public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(userInsert());
+
+    }
+
+    public static String userInsert() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input string: ");
+        return input.nextLine();
+    }
 }
