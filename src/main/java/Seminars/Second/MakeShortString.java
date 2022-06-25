fddbb1720696a26f7930463773b1bd357b7c7d89
Slava Dobrov <dobrov.slava@yandex.ru>
2022-06-25 21:10:54 +0300

    Tried to add method from another class (Palindrome). It was successful.

---
+++ src/main/java/Seminars/Second/MakeShortString.java
@@ -0,0 +1,14 @@
+/**
+ * Сократить строку вида aaabbbssskkk до a3b3s3k3
+ */
+package Seminars.Second;
+
+public class MakeShortString {
+
+    public static void main(String[] args) {
+
+        String newString = Palindrome.userInsert();
+        System.out.println(newString);
+    }
+
+}
