7fd94ddf6f5668a144d19704cfd74d1b2b09cd8c
Slava Dobrov <dobrov.slava@yandex.ru>
2022-06-25 21:49:11 +0300

    Added method which returns short string. Program is working well.

--- src/main/java/Seminars/Second/MakeShortString.java
+++ src/main/java/Seminars/Second/MakeShortString.java
@@ -3,12 +3,39 @@
  */
 package Seminars.Second;
 
+import java.util.Objects;
+
 public class MakeShortString {
 
     public static void main(String[] args) {
 
         String newString = Palindrome.userInsert();
         System.out.println(newString);
+        System.out.println(makeShort(newString));
     }
 
+    public static String makeShort(String str) {
+
+        String[] strArray = str.split("");
+        String resultLine = "";
+        int counter = 1;           // variable which count how long row with the same symbols.
+        int count = 1;             // variable for while.
+        while(count < strArray.length) {
+            if(Objects.equals(strArray[count], strArray[count - 1])) {
+                counter = counter+1;
+            }
+            else if(counter == 1) {
+                resultLine = resultLine + strArray[count-1];
+            }
+            else {
+                resultLine = resultLine + strArray[count-1] + counter;
+                counter = 1;
+            }
+            if(count == strArray.length-1) {
+                resultLine = resultLine + strArray[count] + counter;
+            }
+            count++;
+        }
+        return resultLine;
+    }
 }
