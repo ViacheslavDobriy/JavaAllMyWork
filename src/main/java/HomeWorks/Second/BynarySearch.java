67662e7b4f69f6248f2f8cdd1ca6cc0d8c6dcc63
Slava Dobrov <dobrov.slava@yandex.ru>
2022-06-26 21:35:54 +0300

    Added method with sorting array. I used Insertion Sort.

--- src/main/java/HomeWorks/Second/BynarySearch.java
+++ src/main/java/HomeWorks/Second/BynarySearch.java
@@ -10,11 +10,42 @@ public class BynarySearch {
 
     public static void main(String[] args) {
     int[] generatedArray = CreateArray();
+    ShowArray(generatedArray);
+    generatedArray = SortArray(generatedArray);
+    ShowArray(generatedArray);
     }
 
     public static int[] CreateArray() {
-        int[] array = new int[(int) ((Math.random())*100)+20];
-        System.out.printf("%d - length of array", array.length);
+        int[] array = new int[(int) ((Math.random())*20)+20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) ((Math.random())*20)+20;
+        }
         return array;
+    }
+
+    public static int[] SortArray(int[] array) {
+        for (int left = 0; left < array.length; left++) {
+            int value = array[left];
+            int i = left - 1;
+            for (;i >=0;i--) {
+                if (value < array[i]) {
+                    array[i+1] = array[i];
+                } else {
+                    break;
+                }
+            }
+            array[i+1] = value;
+        }
+        return array;
+    }
+
+
+    public static void ShowArray(int[] array) {
+        int count = 0;
+        while(count < array.length) {
+            System.out.printf("%d ", array[count]);
+            count++;
+        }
+        System.out.println(" ");
     }
 }
