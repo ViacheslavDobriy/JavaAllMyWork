7b118fb57555d9105497b0b561989d98427bf643
Slava Dobrov <dobrov.slava@yandex.ru>
2022-06-26 20:15:36 +0300

    Added another methods for searching numbers and converting result string to int[]. Class is working well.

--- src/main/java/Seminars/Second/DivisionNumbers.java
+++ src/main/java/Seminars/Second/DivisionNumbers.java
@@ -4,6 +4,7 @@
 
 package Seminars.Second;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DivisionNumbers {
@@ -22,14 +23,25 @@ public class DivisionNumbers {
             maximum = GetNumber();
         }
         System.out.printf("%d and %d - are limits of our searching.\n", Integer.parseInt(minimum), Integer.parseInt(maximum));
-        System.out.println(CreateArray(Integer.parseInt(minimum), Integer.parseInt(maximum)));
+        String resultString = CreateLine(Integer.parseInt(minimum), Integer.parseInt(maximum));
+        int[] resultInt = ConvertStringToInt(resultString);
+        ShowArrayInt(resultInt);
     }
 
+    /**
+     * Метод, который предоставляет пользователю ввести значение
+     * @return - тип данных String
+     */
     public static String GetNumber() {
         Scanner in = new Scanner(System.in);
         return in.nextLine();
     }
 
+    /**
+     * Метод, который проверяет, является ли введенное пользователем число типом int
+     * @param number - введенное с консоли число
+     * @return - true, если строка может быть преобразована в int; false, если нет.
+     */
     public static boolean CheckUserNumber(String number) {
         try {
             Integer.parseInt(number);
@@ -40,7 +52,13 @@ public class DivisionNumbers {
         return true;
     }
 
-    public static String CreateArray(int min, int max) {
+    /**
+     * Метод, который создает строку, в которой находятся элементы, которые удовлетворяют условию задачи
+     * @param min - нижняя граница поиска
+     * @param max - верхняя граница поиска
+     * @return - возвращает строку с необходимыми значениями
+     */
+    public static String CreateLine(int min, int max) {
         int[] fullArray = new int[max-min+1];
         for(int i = 0; i < fullArray.length; i++) {
             fullArray[i] = min + i;
@@ -54,6 +72,11 @@ public class DivisionNumbers {
         return result;
     }
 
+    /**
+     * Метод, который складывает цифры внутри числа
+     * @param number - элемент массива, полученный из метода CreateLine()
+     * @return - возвращает сумму цифр элемента массива
+     */
     public static int LookingFor(int number) {
         float floatNumber = (float) number;
         int sumOfDigits = 0;
@@ -65,4 +88,31 @@ public class DivisionNumbers {
         return sumOfDigits;
     }
 
+    /**
+     * Метод, который конвертирует результирующую строку в массив целочисленных значений
+     * @param str - полученная строка из метода CreateLine()
+     * @return - возвращает int[] из параметра.
+     */
+    public static int[] ConvertStringToInt(String str) {
+        String[] resultArray;
+        resultArray = str.split(" ");
+        String[] items = Arrays.toString(resultArray).replaceAll("\\[","").replaceAll("\\]", "").replaceAll("\\s","").split(",");
+        int[] results = new int[items.length];
+        for(int i = 0; i < items.length; i++) {
+            results[i] = Integer.parseInt(items[i]);
+        }
+        return results;
+    }
+
+    /**
+     * Метод, который выводит на экран элементы массива
+     * @param array - результирующий массив
+     */
+    public static void ShowArrayInt(int[] array) {
+        int count = 0;
+        while(count < array.length) {
+            System.out.printf("%d ", array[count]);
+            count++;
+        }
+    }
 }
