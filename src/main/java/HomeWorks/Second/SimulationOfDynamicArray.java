00ae550e922f7118c69106229babedcb4889b893
Slava Dobrov <dobrov.slava@yandex.ru>
2022-06-29 23:29:49 +0300

    Added methods from last homework.

--- src/main/java/HomeWorks/Second/SimulationOfDynamicArray.java
+++ src/main/java/HomeWorks/Second/SimulationOfDynamicArray.java
@@ -5,9 +5,15 @@
 
 package HomeWorks.Second;
 
+import java.util.Scanner;
+
 public class SimulationOfDynamicArray {
     public static void main(String[] args) {
 
+        String userNumber = GetNumber();
+        if(isCorrect(userNumber)) {
+
+        }
     }
 
     public static int[] CreateNewArray(int[] array, int newNumber) {
@@ -18,4 +24,27 @@ public class SimulationOfDynamicArray {
         newArray[array.length] = newNumber;
         return newArray;
     }
+
+    static String GetNumber(){
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Insert integer number - i will make triangular number!");
+        return iScanner.nextLine();
+    }
+
+    static boolean isCorrect(String line){
+        try {
+            if(Integer.parseInt(line)>0)
+                return true;
+        }
+        catch (NumberFormatException e){
+            return false;
+        }
+        return false;
+    }
+
+    static int TriangularNumber(String checkedNumber){
+        float intNumber = Float.parseFloat(checkedNumber);
+        return Math.round(intNumber/2*(intNumber+1));
+    }
+
 }
