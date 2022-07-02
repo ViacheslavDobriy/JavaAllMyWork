f5c7fa2cf2f9a0c7c61740abae3ae4f4b9922c21
ViacheslavDobriy <dobrov.slava@yandex.ru>
2022-07-02 14:42:20 +0800

    I searched sites for finding correct and easy solution for tower of Hanoi. And find it. Program is working well.

--- src/main/java/HomeWorks/Second/TowerOfHanoi.java
+++ src/main/java/HomeWorks/Second/TowerOfHanoi.java
@@ -5,31 +5,29 @@ package HomeWorks.Second;
 
 public class TowerOfHanoi {
     public static void main(String[] args) {
-        int maxRingCount = 3;
-        int[] towers = new int[]{maxRingCount,0,0};
-        MoveRing(towers, maxRingCount, 0, 1,2,0);
+
+        int nRings = 3;
+        String StepsToSolution = MoveRings(nRings,1,3);
+
+        for(String step : StepsToSolution.split(";")){
+            System.out.println(step);
+        }
     }
 
-    public static void MoveRing(int[] tower, int maxRingCount, int topColumn, int buferColumn, int nextTop, int rootColumn) {
+    public static String MoveRings(int nRings, int fromPeg, int toPeg) {
+        if(nRings == 1) {
+            return fromPeg + "->" + toPeg + ";";
+        } else {
+            String sol1, sol2, sol3;
+            int helpPeg = 6 - fromPeg - toPeg;
+            sol1 = MoveRings(nRings - 1, fromPeg, helpPeg);
+            sol2 = fromPeg + "->" + toPeg + ";";
+            sol3 = MoveRings(nRings - 1, helpPeg, toPeg);
 
-        if (tower[1]!=maxRingCount || tower[2]!=maxRingCount) {
-            System.out.printf("Move from %d to %d\n", topColumn, buferColumn);
-            tower[topColumn] = tower[topColumn] - 1;
-            tower[buferColumn] = tower[buferColumn] + 1;
-            System.out.printf("Move from %d to %d\n", topColumn, nextTop);
-            tower[topColumn] = tower[topColumn] - 1;
-            tower[nextTop] = tower[nextTop] + 1;
-            System.out.printf("Move from %d to %d\n", buferColumn, nextTop);
-            tower[buferColumn] = tower[buferColumn] - 1;
-            tower[nextTop] = tower[nextTop] + 1;
-            if(tower[rootColumn]==1 && tower[buferColumn]==0){
-                System.out.printf("Move from %d to %d\n", rootColumn, buferColumn);
-                tower[rootColumn] = tower[rootColumn] - 1;
-                tower[buferColumn] = tower[buferColumn] + 1;
-                rootColumn = buferColumn;
-            }
-            MoveRing(tower, maxRingCount, nextTop, topColumn, buferColumn, rootColumn);
+            return sol1 + sol2 + sol3;
 
-            }
+        }
+
+
         }
 }
