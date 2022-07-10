53df880fb45f534c5f86d81e775870c71e4637ff
Slava Dobrov <dobrov.slava@yandex.ru>
2022-07-10 17:03:10 +0300

    Commented code from seminar 4

--- src/main/java/Seminars/Seminar4/People.java
+++ src/main/java/Seminars/Seminar4/People.java
@@ -1,18 +1,18 @@
-package Seminars.Seminar4;
-
-public class People {
-
-    private String name;
-    private String workName;
-    private String number;
-    private String anotherNumber;
-
-    public People(String name, String workName) {
-        this.name=name;
-        this.workName = workName;
-    }
-
-    public String getName() {
-        return "sss";
-    }
-}
+//package Seminars.Seminar4;
+//
+//public class People0 {
+//
+//    private String name;
+//    private String workName;
+//    private String number;
+//    private String anotherNumber;
+//
+//    public People0(String name, String workName) {
+//        this.name=name;
+//        this.workName = workName;
+//    }
+//
+//    public String getName() {
+//        return "sss";
+//    }
+//}
