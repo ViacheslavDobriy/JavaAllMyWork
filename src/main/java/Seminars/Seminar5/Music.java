b3c8dba41a1aa31ffc922a0f19e42ef0dd59f4ad
Slava Dobrov <dobrov.slava@yandex.ru>
2022-07-12 21:52:59 +0300

    I added method CreateGenres() and method CreateSongNames(). I put it in Map<String, Map<String, Integer>>. And from now I can, and I need sort my map.

--- src/main/java/Seminars/Seminar5/Music.java
+++ src/main/java/Seminars/Seminar5/Music.java
@@ -19,57 +19,101 @@ public class Music {
 
     public static void main(String[] args) {
 
-        Map<String, Map<String, Integer>> playList = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-
-            Map<String, Integer> temp = new HashMap<>();
-            temp.put(genStr(),(int) (Math.random()*1001)+1);
-            playList.put(genStr(),temp);
-
-        }
+        List<String> genres = CreateGenres();
+        Map<String, Integer> songRates = CreateSongNames();
+        Map<String, Map<String, Integer>> playList = FillMap(genres, songRates);
         System.out.println(playList);
-        Sort(playList);
-//        for (HashMap item:playList) {
-//
-//        }
 
     }
 
-    public static String genStr () {
-        int size = (int) (Math.random()*10)+5;
-        String genStr = "";
-        for (int i = 0; i < size; i++) {
+    public static List<String> CreateGenres() {
 
-            genStr = genStr + (char)((Math.random() * 26) + 97);
+        List<String> genres = new ArrayList<>();
+        genres.add("Pop");
+        genres.add("Hip-Hop");
+        genres.add("Rap");
+        genres.add("Classic");
+        genres.add("Rock");
+        genres.add("DupStep");
+        return genres;
 
-        }
-        return genStr;
     }
 
-    public static void Sort(Map<String, Map<String, Integer>> noSorted) {
-        List<String> howManyListened = new ArrayList<>();
-        for (Map.Entry<String, Map<String, Integer>> item:noSorted.entrySet()) {
-            String[] music = item.getValue().keySet().toArray(new String[item.getValue().keySet().size()]);
-            for (String str: music) {
-                System.out.println(str);
-            }
-            int temp = 0;
-            for (int i = 0; i < music.length; i++) {
-                for (int j = i; j < music.length; j++) {
-                    int max = item.getValue().get(music[j]);
-                    if(max<item.getValue().get(music[j])) {
+    public static Map<String, Integer> CreateSongNames() {
 
-                        temp = j;
-                        max = item.getValue().get(music[j]);
+        Map<String, Integer> songNames = new HashMap<>();
+        songNames.put("Boshki Dymyatsa", (int) ((Math.random() * 1001) + 1));
+        songNames.put("Sinimi Zheltymi Krasnymi", (int) ((Math.random() * 1001) + 1));
+        songNames.put("Highway To Hell", (int) ((Math.random() * 1001) + 1));
+        songNames.put("Sonet", (int) ((Math.random() * 1001) + 1));
+        songNames.put("Rayda", (int) ((Math.random() * 1001) + 1));
+        songNames.put("Since U Been Gone", (int) ((Math.random() * 1001) + 1));
+        songNames.put("La Grange", (int) ((Math.random() * 1001) + 1));
+        songNames.put("My Heart", (int) ((Math.random() * 1001) + 1));
+        songNames.put("Someday", (int) ((Math.random() * 1001) + 1));
+        songNames.put("Shape Of You", (int) ((Math.random() * 1001) + 1));
+        return songNames;
 
+    }
 
-                    } else {
+    public static Map<String, Map<String, Integer>> FillMap(List<String> genres, Map<String, Integer> songRates) {
 
+        Map<String, Map<String, Integer>> playList = new HashMap<>();
+
+        for (int i = 0; i < songRates.size(); i++) {
+
+            List<String> keyList = new ArrayList<>(songRates.keySet());
+            List<Integer> valueList = new ArrayList<>(songRates.values());
+            Map<String, Integer> temp = new HashMap<>();
+
+            List<String> keyPlayList = new ArrayList<>(playList.keySet());
+            temp.put(keyList.get(i), valueList.get(i));
+
+            if (playList.isEmpty()) {
+                playList.put(genres.get((int) (Math.random() * 6)), temp);
+            } else {
+                boolean find = false;
+                int forListGenre = (int) (Math.random() * 6);
+                for (int j = 0; j < playList.size(); j++) {
+
+                    if (Objects.equals(genres.get(forListGenre), keyPlayList.get(j))) {
+
+                        playList.get(genres.get(forListGenre)).put(keyList.get(i), valueList.get(i));
+                        find = true;
                     }
                 }
-                howManyListened.add(music[temp]);
+                if (!find) {
+                    playList.put(genres.get(forListGenre), temp);
+                }
             }
         }
-        System.out.println(howManyListened);
+        return playList;
     }
 }
+//    public static void Sort(Map<String, Map<String, Integer>> noSorted) {
+//        List<String> howManyListened = new ArrayList<>();
+//        for (Map.Entry<String, Map<String, Integer>> item:noSorted.entrySet()) {
+//            String[] music = item.getValue().keySet().toArray(new String[item.getValue().keySet().size()]);
+//            for (String str: music) {
+//                System.out.println(str);
+//            }
+//            int temp = 0;
+//            for (int i = 0; i < music.length; i++) {
+//                for (int j = i; j < music.length; j++) {
+//                    int max = item.getValue().get(music[j]);
+//                    if(max<item.getValue().get(music[j])) {
+//
+//                        temp = j;
+//                        max = item.getValue().get(music[j]);
+//
+//
+//                    } else {
+//
+//                    }
+//                }
+//                howManyListened.add(music[temp]);
+//            }
+//        }
+//        System.out.println(howManyListened);
+//    }
+
